b5e4455d722d46722663e7f43c354e128e9fe040
RaulToainga34 <rabetomo542@gmail.com>
2026-04-18 12:03:18 -0500

    Vidas y Puntaje

--- utiles.js
+++ utiles.js
@@ -6,4 +6,8 @@ function generarAleatorio(min,max){
     return numeroEntero
 }
 
+function mostrarEnSpan(idSpan, valor){
+    let componente = document.getElementById(idSpan);
+componente.textContent=valor;
+}
 
