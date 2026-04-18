9a6a6a787ccb967361538975cf0803ed89933f3c
RaulToainga34 <rabetomo542@gmail.com>
2026-04-18 16:10:48 -0500

    Se agrego GANADOR

--- limones.js
+++ limones.js
@@ -74,6 +74,16 @@ function detectarAtrapado(){
         aparecerLimon();
         puntaje = puntaje+1;
         mostrarEnSpan("txtPuntaje",puntaje);
+        if(puntaje==3){
+            velocidadCaida = 150;
+        }
+        if(puntaje==6){
+            velocidadCaida = 100;
+        }
+        if(puntaje==10){
+            alert("GANADOR");
+            alert("Ya tienes limones ahora limpia el microondas");
+        }
     }
 }
 
