2308faa777ef476e928f22b0b454e4d5765da14c
RaulToainga34 <rabetomo542@gmail.com>
2026-04-18 10:36:40 -0500

    Se agrego el movimiento a la izquierda

--- limones.js
+++ limones.js
@@ -5,6 +5,8 @@ const ALTURA_SUELO=30;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
+let personajeX = canvas.width/2;
+
 function dibujarSuelo(){
     ctx.fillStyle = "#B52121";
     ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
@@ -12,10 +14,25 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle = "#20bf5a";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+}
+
+function moverIzquierda(){
+    personajeX = personajeX-10;
+    actualizarPantalla();
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 }
