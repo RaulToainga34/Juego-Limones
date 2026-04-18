0cf65588c2b3a7f8815df3e975db72be16823004
RaulToainga34 <rabetomo542@gmail.com>
2026-04-18 10:25:05 -0500

    Se crearon las funciones dibujarSuelo, dibujarPersonaje e iniciar, se agregaron constantes y se llama la funcion iniciar con el onload

--- limones.js
+++ limones.js
@@ -1,2 +1,21 @@
 let canvas=document.getElementById("areaJuego");
-let ctx=canvas.getContext("2d")
+let ctx=canvas.getContext("2d");
+
+const ALTURA_SUELO=30;
+const ALTURA_PERSONAJE=60;
+const ANCHO_PERSONAJE=40;
+
+function dibujarSuelo(){
+    ctx.fillStyle = "#B52121";
+    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+}
+
+function dibujarPersonaje(){
+    ctx.fillStyle = "#20bf5a";
+    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+}
+
+function iniciar(){
+    dibujarSuelo();
+    dibujarPersonaje();
+}
